47a28cc24b8b64a397287e8bac41a4ef9f18d3d5
Navjot Singh Virk <virksaabnavjot@gmail.com>
2016-12-25 14:32:37 +0000

    Adding some style to cars plot

--- src/RStudio/ProducingSimpleGraphsWithR.R
+++ src/RStudio/ProducingSimpleGraphsWithR.R
@@ -2,8 +2,15 @@
 #http://www.harding.edu/fmccown/r/
 
 # Define the cars vector with 5 values
-cars <- c(6, 4, 9, 23, 25)
+cars <- c(6, 15, 9, 23, 25)
 
 # Graph the cars vector with all defaults
 plot(cars)
+
+# Graph cars using green points overlayed by a line
+plot(cars, type = "o", col = "green")
+
+# Create a title with a Blue, bold/italic font
+title(main="Autos", col.main="blue", font.main=4)
+
 
