1b8f5793a6a2e83504496d8b236b7f2c212cf4f1
Navjot Singh Virk <virksaabnavjot@gmail.com>
2016-12-25 14:23:52 +0000

    Working with Plots in R

---
+++ src/RStudio/ProducingSimpleGraphsWithR.R
@@ -0,0 +1,9 @@
+#References: This code is done with the help of this tutorial
+#http://www.harding.edu/fmccown/r/
+
+# Define the cars vector with 5 values
+cars <- c(6, 4, 9, 23, 25)
+
+# Graph the cars vector with all defaults
+plot(cars)
+
