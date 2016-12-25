2266d9ed12d3ae81f40b0606bffd05a0e5ac313b
Navjot Singh Virk <virksaabnavjot@gmail.com>
2016-12-25 14:43:15 +0000

    working with 2 vectors and plotting a graph

--- src/RStudio/ProducingSimpleGraphsWithR.R
+++ src/RStudio/ProducingSimpleGraphsWithR.R
@@ -14,3 +14,20 @@ plot(cars, type = "o", col = "green")
 title(main="Autos", col.main="blue", font.main=4)
 
 
+#Now let's add a red line for trucks and specify the y-axis range directly 
+#so it will be large enough to fit the sports cars data:
+
+
+# Define 2 vectors
+cars <- c(6, 15, 9, 23, 25)
+sports_cars <- c(2, 25, 4, 29, 12)
+
+# Graph cars using a y axis that ranges from 0 to 30
+plot(cars, type="o", col="green", ylim=c(0,30))
+
+# Graph sports_cars with red dashed line and square points
+lines(sports_cars, type="o", pch=22, lty=2, col="red")
+
+# Create a title with a blue, bold/italic font
+title(main="Autos", col.main="blue", font.main=4)
+
